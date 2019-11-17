30e8dd16b5c41eddd90888bf0d41fa8795f92015
Graham Flaspoehler <grahamflas@gmail.com>
2019-11-17 14:06:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize( string ) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize( string ) {
+    return string.replace(/[^a-zA-Z0-9'-\s]/gi, "")
+  }
+
+  static titleize( string ) {
+    let titleArray = [];
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    let words = string.split(" ");
+
+    for( let i=0; i < words.length; i++ ) {
+      if ( i == 0 ) {
+        titleArray.push( this.capitalize( words[i] ) );
+      } else if ( exceptions.includes( words[i].toLowerCase() ) ) {
+        titleArray.push( words[i] );
+      } else {
+        titleArray.push( this.capitalize( words[i] ) )
+      }
+    }
+
+    return titleArray.join(" ");
+  }
 }
